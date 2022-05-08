51857036988c7432424c029b8a3fa7c4b8be2add
Bethany Weisberg <bweisberg6@gmail.com>
2022-05-07 19:04:28 -0700

    Edited Folder Structure

--- CIP Final/uninsured_rates_US.py
+++ CIP Final/uninsured_rates_US.py
@@ -1,12 +1,18 @@
 # Data courtesy of Kaiser Family Foundation
-# 
+# github link here when finished....
 
-import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-
+US_UNINSURED = 'combined.csv'
 
 def main():
+    df = pd.read_csv(US_UNINSURED)
+    df[df.Location=="United States"].transpose().iloc[1:,].plot(title = "Plot of US {INSERT CORRECT NAME} over Time",
+                                                            xlabel="Year", 
+                                                            ylabel="Number")
 
+    
 if __name__ == '__main__':
     main()
