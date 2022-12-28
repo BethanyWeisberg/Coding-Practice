7c194cc1a2419f03924d0abaf698fd1e7eb74a03
Bethany Weisberg <95442334+BethanyWeisberg@users.noreply.github.com>
2022-12-27 16:21:52 -0800

    Update python_dictionary.py

--- Python Practice/python_dictionary.py
+++ Python Practice/python_dictionary.py
@@ -6,17 +6,17 @@ python_definitions = {
     'if statement': 'conditional execution',
 }
 
-python_definitions['for loop'] = 'used for iterating over a sequence'
-python_definitions['set'] = 'used to store unique items in a single variable'
-python_definitions['items()'] = 'used to return a list of key-value pairs'
-python_definitions['f string'] = 'literal string which contains expressions inside braces that are replaced by values in the output'
-python_definitions['nesting'] = 'a list or dictionary that contains additional list or dictionary'
-
 for key,value in python_definitions.items():
     if key == 'list':
         print(f'A {key} is {value}.')
     else:
         print(f'A {key} is a {value}.')
  
+python_definitions['for loop'] = 'used for iterating over a sequence'
+python_definitions['set'] = 'used to store unique items in a single variable'
+python_definitions['items()'] = 'used to return a list of key-value pairs'
+python_definitions['f string'] = 'literal string which contains expressions inside braces that are replaced by values in the output'
+python_definitions['nesting'] = 'a list or dictionary that contains additional list or dictionary'
+
 for name in python_definitions.keys():
     print(name)
