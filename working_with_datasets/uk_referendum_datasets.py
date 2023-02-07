24ed4740b1f9c320fc26c3cb77e3c33ff9a921bd
Bethany Weisberg <bethanyweisberg@curative.com>
2023-02-06 22:34:43 -0800

    aggregations update

--- working_with_datasets/uk_referendum_datasets.py
+++ working_with_datasets/uk_referendum_datasets.py
@@ -129,33 +129,23 @@ regions = set([area['Region'] for area in merged_list])
 # add electorate sum total for each region
 region_dict = {}
 
-total_region_sum = 0
+total_region_sum = sum(area['Electorate'] for area in merged_list)
+region_aggregations = []
 for region in regions:
     region_sum = sum([area['Electorate'] for area in merged_list if area['Region'] ==  region])
-    total_region_sum += region_sum
-    region_dict[region] = {
-        "region_sum": region_sum,
+    region_dict= {
+        "region": region,
+        "electorate_sum": region_sum,
+        "electorate_percentage": "{:.2%}".format(region_sum/total_region_sum)
     }
-
-for region in regions:
-    region_dict[region]["percentage_of_total"] = "{:.2%}".format((region_dict[region]["region_sum"]/total_region_sum))
+    region_aggregations.append(region_dict)
+print(region_aggregations)
 
 filename = 'uk_referendum_aggregations.csv'
 fields = ['region','electorate_sum','electorate_percentage']
 with open(filename, 'w', newline='') as fp:
     csv_w = csv.DictWriter(fp,fieldnames=fields)
     csv_w.writeheader()
-        # csv_w.writerow({field: region_dict[region].get(field) or region for field in region_dict})
-    for region, info in region_dict.items():
-        # for key, value in info.items():
-        csv_w.writerow(dict(zip(fields, (region,list(info.values())))))
+    for item in region_aggregations:
+        csv_w.writerow(item)
 fp.close()
-    # csv_columns = ['Question', 'UserID', 'Answer']
-    # filename = "output.csv"
-    # with open(filename, "w") as fl:
-    #     w = csv.DictWriter(fl, fieldnames=csv_columns, lineterminator='\n')
-    #     w.writeheader()
-    #     for question, data in finalDict.items()
-    #         for item in data:
-    #             for key, value in item.items():
-    #                 w.writerow(dict(zip(csv_columns, (question, user, answer))))
